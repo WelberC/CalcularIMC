577b594e5dc43b1fab06b65751dd93acc8d1c378
WelberC <welber728@gmail.com>
2022-04-23 23:29:01 -0300

    Calculadora IMC

---
+++ CalculoIMC/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace CalculoIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.calcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Weight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Heigth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // calcular
+            // 
+            this.calcular.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.calcular.BackColor = System.Drawing.Color.LightSalmon;
+            this.calcular.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.calcular.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.calcular.Location = new System.Drawing.Point(159, 247);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(195, 60);
+            this.calcular.TabIndex = 0;
+            this.calcular.Text = "CALCULAR IMC";
+            this.calcular.UseVisualStyleBackColor = false;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(54, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Peso";
+            // 
+            // Weight
+            // 
+            this.Weight.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Weight.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Weight.Location = new System.Drawing.Point(159, 108);
+            this.Weight.Name = "Weight";
+            this.Weight.PlaceholderText = "Digite seu peso";
+            this.Weight.Size = new System.Drawing.Size(195, 35);
+            this.Weight.TabIndex = 2;
+            this.Weight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Weight_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(54, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Altura";
+            // 
+            // Heigth
+            // 
+            this.Heigth.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Heigth.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Heigth.Location = new System.Drawing.Point(159, 174);
+            this.Heigth.Name = "Heigth";
+            this.Heigth.PlaceholderText = "Digite sua altura";
+            this.Heigth.Size = new System.Drawing.Size(195, 35);
+            this.Heigth.TabIndex = 4;
+            this.Heigth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Heigth_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(159, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Calcule seu IMC";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(107, 339);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(297, 21);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Desenvolvido por Welber Caetano Santos";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(510, 410);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Heigth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Weight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcular);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de IMC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Weight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Heigth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ CalculoIMC/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+
+            //Primeiro tratamento de erro: Impede o usuário de tentar calcular com um dos campos vazios
+            if (String.IsNullOrEmpty(Weight.Text) || (String.IsNullOrEmpty(Heigth.Text)))
+            {
+                MessageBox.Show("Digite valores válidos para sua altura e peso!");
+            }
+
+            else
+            {
+                //Se estiver tudo certo com o programa, e não tiver nenhum erro, ai sim, o IMC será calculado.
+
+
+                //Informações que precisamos: Peso e Altura. A variável IMC vai calcular o IMC e armazená-lo
+                double peso, altura, imc;
+
+                peso = Convert.ToDouble(Weight.Text);
+                altura = Convert.ToDouble(Heigth.Text);
+
+                imc = peso / (altura * altura);
+
+                if (imc < 18.5)
+                {
+                    MessageBox.Show("Seu IMC é de: " + imc.ToString("F") + " E você está abaixo do peso!");
+                }
+
+                else if (imc < 25)
+                {
+                    MessageBox.Show("Seu IMC é de: " + imc.ToString("F") + " E você está com o peso normal!");
+                }
+
+                else if (imc < 30)
+                {
+                    MessageBox.Show("Seu IMC é de: " + imc.ToString("F") + " E você está com excesso de peso!");
+                }
+
+                else if (imc < 35)
+                {
+                    MessageBox.Show("Seu IMC é de: " + imc.ToString("F") + " E você está com obesidade grau 1!");
+                }
+
+                else if (imc < 40)
+                {
+                    MessageBox.Show("Seu IMC é de: " + imc.ToString("F") + " E você está com obesidade grau 2!");
+                }
+
+                else
+                {
+                    MessageBox.Show("Seu IMC é de: " + imc.ToString("F") + " E você está com obesidade grau 3!");
+                }
+
+            }
+            
+        }
+
+        private void Heigth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Isso aqui é a restrição da TextBox. Com isso, só números, vírgulas e backspaces podem ser digitados
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08 && e.KeyChar !=44)
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+        }
+
+        private void Weight_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Se a tecla digitada não for número e nem backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08 && e.KeyChar != 44)
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+
+            //Referência dessa última parte:
+            //http://tutoriaiscsharp.blogspot.com/2012/05/como-fazer-o-textbox-permitir-apenas.html
+
+            //Basicamente, é isso aqui! Muito obrigado por ler o código, ou fazer o download do programa.
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //Abre meu site
+            System.Diagnostics.Process.Start("C:/Program Files/Google/Chrome/Application/chrome.exe", "https://welberc.github.io/CurriculoEmSite/");
+        }
+    }
+}
